2391904425d148fb3e05172e7dd27ec94490484b
mayankbharti14 <mayyankkb@gmail.com>
2025-07-24 16:23:42 +0530

    Update sorting algorithms.cpp

--- sorting algorithms.cpp
+++ sorting algorithms.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class SortingAlgos{
     public:
-    auto re(auto& v){
+    auto sortpairincdec(auto& v){
         sort(v.begin(),v.end(),[](auto &a, auto &b){
             if(a.first!=b.first)return (a.first>b.first);
             else return(a.second<b.second);
